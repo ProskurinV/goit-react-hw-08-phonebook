4656dc307bce2adfa0d9304adf3698d972b3f83a
ProskurinV <proskyrinv.g@gmail.com>
2022-11-09 14:37:30 +0200

    contactsAdd

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,14 +1,10 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
+import contacts from '../data/contacts.json';
 
 const contactsInitialState = {
-  contactList: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contactList: [...contacts],
 };
 
 const contactsSlice = createSlice({
