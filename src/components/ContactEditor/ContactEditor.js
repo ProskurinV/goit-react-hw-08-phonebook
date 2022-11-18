f64c597a8ab33d7293259e6e568058a8202c7dfa
ProskurinV <proskyrinv.g@gmail.com>
2022-11-18 17:28:19 +0200

    noStyle

--- src/components/ContactEditor/ContactEditor.js
+++ src/components/ContactEditor/ContactEditor.js
@@ -9,22 +9,8 @@ import { addContact } from 'redux/contacts/operations';
 import { selectContacts } from 'redux/contacts/selectors';
 
 let schema = yup.object().shape({
-  name: yup
-    .string()
-    .matches(
-      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-    )
-    .required('Please, enter name'),
-  number: yup
-    .string()
-    .min(7)
-    .max(12)
-    .matches(
-      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
-      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-    )
-    .required('Please, enter correct number'),
+  name: yup.string().required('Please, enter name'),
+  number: yup.string().min(7).required('Please, enter correct number'),
 });
 
 const initialValues = {
